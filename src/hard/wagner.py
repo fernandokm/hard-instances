c73111dd68fe3c86c4b55181ef0f7ecc76ab35f3
Fernando Matsumoto <ferkmatsumoto@gmail.com>
2023-06-01 12:06:35 +0200

    Split generation and sampling logic

--- src/hard/wagner.py
+++ src/hard/wagner.py
@@ -2,10 +2,9 @@ from typing import Any, Literal, overload
 
 import numpy as np
 import torch
+from hard.instance_sampler import InstanceSampler
 from solvers.base import Solver
 from torch import nn
-from torch.distributions import Bernoulli, Categorical
-from torch.nn import functional as F
 from torch.optim import AdamW
 from tqdm.auto import trange
 
@@ -15,70 +14,49 @@ class Wagner:
 
     def __init__(
         self,
-        instance_shape: tuple[int, ...],
-        instance_values: list[int] | int = 2,
+        shape: tuple[int, ...],
+        sampler: InstanceSampler | None = None,
         instances_per_epoch: int = 1000,
         frac_train: float = 0.07,
         frac_survival: float = 0.06,
-        epochs: int = 1000,
         lr: float = 1e-4,
-        activation: Literal["sigmoid", "softmax"] | None = None,
         device: str = "cpu",
     ) -> None:
-        self.device = device
-
-        self.instance_shape = instance_shape
+        self.instance_shape = shape
+        self.instance_shape_flat = int(np.prod(shape))
+        self.sampler = sampler or InstanceSampler()
         self.instances_per_epoch = instances_per_epoch
         self.frac_train = frac_train
         self.frac_survival = frac_survival
-        self.epochs = epochs
         self.lr = lr
+        self.device = device
+
+        self.sampler.set_device(device)
+
         self.memory = []
         self.scores_per_generation = []
-
-        if isinstance(instance_values, int):
-            self.instance_values_dim = instance_values
-            self.instance_values = None
-        else:
-            self.instance_values_dim = len(instance_values)
-            self.instance_values = torch.as_tensor(instance_values).to(self.device)
-
-        if activation == "sigmoid" and self.instance_values_dim > 2:
-            msg = (
-                f"Cannot use activation 'sigmoid' with more than "
-                f"two instance_values: {instance_values}"
-            )
-            raise ValueError(msg)
-        if activation == "softmax" or self.instance_values_dim > 2:
-            self.activation = "softmax"
-        else:
-            self.activation = "sigmoid"
-
-        self.instance_size_flat = 1
-        for dim in self.instance_shape:
-            self.instance_size_flat *= dim
 
         self.reset()
 
     def generate(self, n: int = 1, transform_values: bool = True) -> torch.Tensor:
-        instances = []
         with torch.no_grad():
-            obs = torch.zeros((n, self.instance_size_flat * 2)).to(self.device)
-            for i in range(self.instance_size_flat):
-                obs[:, self.instance_size_flat + i] = 1
+            obs = torch.zeros((n, self.instance_shape_flat * 2), device=self.device)
+            output = torch.zeros((n, self.instance_shape_flat), dtype=torch.long, device=self.device)
+            for i in range(self.instance_shape_flat):
+                # Temporarily update the mask for sampling
+                obs[:, self.instance_shape_flat + i] = 1
                 logits = self.net(obs)
-                obs[:, self.instance_size_flat + i] = 0
+                obs[:, self.instance_shape_flat + i] = 0
 
-                if self.activation == "sigmoid":
-                    dist = Bernoulli(logits=logits.view(n, -1))
-                else:
-                    dist = Categorical(logits=logits.view(n, -1))
-                sampled = dist.sample().int()
-                if transform_values and self.instance_values is not None:
-                    sampled = self.instance_values[sampled]
-                instances.append(sampled)
+                sampled = self.sampler.sample_logits(
+                    logits.view(n, -1), transform_values=transform_values
+                )
+                obs[:, i] = sampled
+                output[:, i] = sampled
 
-        return torch.cat(instances).view((n, *self.instance_shape))
+            # return a copy of the tensor in order not to leak
+            # the right half of obs (the mask)
+            return output.view(n, *self.instance_shape)
 
     @overload
     def train(
@@ -88,7 +66,7 @@ class Wagner:
         *,
         return_results: Literal[True] = True,
         return_instances: bool = True,
-        generations: int | None = None,
+        generations: int = 1000,
         instances_per_generation: int | None = None,
     ) -> list[dict[str, Any]]:
         pass
@@ -101,7 +79,7 @@ class Wagner:
         *,
         return_results: Literal[False] = False,
         return_instances: bool = True,
-        generations: int | None = None,
+        generations: int = 1000,
         instances_per_generation: int | None = None,
     ) -> None:
         pass
@@ -113,11 +91,9 @@ class Wagner:
         *,
         return_results: bool = False,
         return_instances: bool = True,
-        generations: int | None = None,
+        generations: int = 1000,
         instances_per_generation: int | None = None,
     ):
-        if generations is None:
-            generations = self.epochs
         if instances_per_generation is None:
             instances_per_generation = self.instances_per_epoch
         results = [] if return_results else None
@@ -125,25 +101,21 @@ class Wagner:
         it = trange(generations, desc="Generations")
         for _ in it:
             pop = self.generate(instances_per_generation, transform_values=False)
-            scores_list = []
+            scores = []
             for i in range(instances_per_generation):
-                inst = pop[i]
-                if self.instance_values is not None:
-                    inst = self.instance_values[inst.to(torch.int32)]
-                inst_np = inst.cpu().numpy()
-                res = solver.solve_instance(inst_np)
-                scores_list.append(res[metric])
+                inst = self.sampler.transform(pop[i]).cpu().numpy()
+                res = solver.solve_instance(inst)
+                scores.append(res[metric])
                 if results is not None:
                     if return_instances:
-                        res["instance"] = inst_np
+                        res["instance"] = inst
                     results.append(res)
 
-            scores = torch.as_tensor(scores_list)
             self.scores_per_generation.append(scores)
             self.train_on_generation(pop, scores)
             all_scores = [x[1] for x in self.memory]
             it.set_postfix(
-                avg_iter_score=scores.mean(dtype=torch.float32).cpu().item(),
+                avg_iter_score=np.mean(scores),
                 avg_pop_score=np.mean(all_scores),
                 max_pop_score=max(all_scores),
                 pop_size=len(all_scores),
@@ -151,24 +123,20 @@ class Wagner:
 
         return results
 
-    def train_on_generation(
-        self, new_instances: torch.Tensor, new_scores: torch.Tensor
-    ):
-        assert new_instances.shape[0] == new_scores.shape[0]
+    def train_on_generation(self, new_instances: torch.Tensor, new_scores: list[float]):
+        assert new_instances.shape[0] == len(new_scores)
         num_instances = new_instances.shape[0]
         new_instances = new_instances.view(num_instances, -1)
 
-        if not (
-            (0 <= new_instances) & (new_instances < self.instance_values_dim)
-        ).all():
+        if not ((0 <= new_instances) & (new_instances < self.sampler.num_values)).all():
             msg = (
                 "The new_instances should be untransformed (i.e. their elements should"
-                "be in the range [0, self.instance_values_dim))"
+                "be in the range [0, sampler.num_values))"
             )
             raise ValueError(msg)
 
         for i in range(num_instances):
-            self.memory.append((new_instances[i].clone(), new_scores[i].item()))
+            self.memory.append((new_instances[i].clone(), new_scores[i]))
 
         self.memory.sort(key=lambda x: x[1], reverse=True)
         train_size = int(self.frac_train * len(self.memory))
@@ -183,7 +151,7 @@ class Wagner:
 
     def _fit_crossentropy(self, population: torch.Tensor):
         pop_size = population.shape[0]
-        inst_size = self.instance_size_flat
+        inst_size = self.instance_shape_flat
         states = torch.zeros(
             (pop_size * inst_size, 2 * inst_size),
             device=self.device,
@@ -199,29 +167,23 @@ class Wagner:
             target[start:end] = population[:, i]
 
         preds = self.net(states)
-
-        if self.activation == "sigmoid":
-            loss = F.binary_cross_entropy_with_logits(preds, target)
-        else:
-            loss = F.cross_entropy(preds, target)
+        loss = self.sampler.cross_entropy(preds, target)
 
         self.optimizer.zero_grad(set_to_none=True)
         loss.backward()
         self.optimizer.step()
 
     def reset(self) -> None:
-        out_size = self.instance_values_dim if self.activation == "softmax" else 1
-
         self.net = nn.Sequential(
-            nn.Linear(self.instance_size_flat * 2, 128),
+            nn.Linear(self.instance_shape_flat * 2, 128),
             nn.ReLU(),
             nn.Linear(128, 64),
             nn.ReLU(),
             nn.Linear(64, 4),
             nn.ReLU(),
-            nn.Linear(4, out_size),
+            nn.Linear(4, self.sampler.logits_dim),
         ).to(self.device)
 
-        self.net = torch.jit.script(self.net)
+        self.net = torch.jit.script(self.net)  # type: ignore
 
         self.optimizer = AdamW(self.net.parameters(), lr=self.lr)
