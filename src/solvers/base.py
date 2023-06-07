d78a7dd8fc5cc437c98232386cf41a744bfe9325
Fernando Matsumoto <ferkmatsumoto@gmail.com>
2023-06-07 10:36:59 +0200

    Add solver names

--- src/solvers/base.py
+++ src/solvers/base.py
@@ -1,4 +1,7 @@
+import os
 from abc import ABC, abstractmethod
+from collections.abc import Collection, Iterator
+from multiprocessing import Pool
 from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:
@@ -12,3 +15,7 @@ class Solver(ABC):
         instance: "np.ndarray",
     ) -> dict[str, Any]:
         pass
+
+    @property
+    def name(self):
+        return self.__class__.__name__
